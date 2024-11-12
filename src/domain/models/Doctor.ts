8baa0556d92e4dc5444b94c5ac877d8722ecb286
Ostapciuc Diana Miruna <diana.ostapciuc@gmail.com>
2024-11-12 10:40:20 +0200

    added a standard navbar for general use and separated patient details from the main page

--- src/domain/models/Doctor.ts
+++ src/domain/models/Doctor.ts
@@ -1,11 +1,6 @@
-import { Patient } from "./Patient";
-import { Appointment } from "../Appointment";
-
 export interface Doctor {
     id: string;
     firstName: string;
     lastName: string;
     specialization: string;
-    patients: Patient[];
-    appointments: Appointment[];
   }
